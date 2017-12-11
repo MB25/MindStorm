cd26b2e48a69e1f275a6a8f91ee2576040e427e2
Jonas <kecksohn@gmail.com>
2017-12-11 19:15:19 +0100

    geht

--- MainServer/src/mainServer/MainServer.java
+++ MainServer/src/mainServer/MainServer.java
@@ -1,11 +1,23 @@
 package mainServer;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.rmi.RemoteException;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  * Deals with getting Data from our python Script (which reads input from the Arduino)
@@ -13,57 +25,173 @@ import java.net.Socket;
 **/
 public class MainServer {
 	
+	private Robot robot;
+	private boolean success = false;
+	private int[] values;
+	
+	public void initiate() {
+		
+		
+		JFrame frame = new JFrame("Connect Prototype");
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		JLabel label = new JLabel("Please enter Robot IP");
+		JButton connect = new JButton();
+		connect.setText("Connect");
+		panel.add(label);
+		panel.add(connect);
+		JTextField connectfield = new JTextField(10);
+	    frame.add(connectfield, BorderLayout.NORTH);
+		frame.add(panel);
+		frame.setSize(300, 300);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		connect.addActionListener(new ActionListener() { 
+			public void actionPerformed(ActionEvent e) {
+				String ip = connectfield.getText();
+				label.setText("connecting...");
+				try {
+					robot = new Robot(ip);
+					success = true;
+				} 
+				catch (Exception e1) {
+					label.setText("Please try again! "+e1.getMessage());
+				}
+				  
+				if (success) {
+					frame.dispose();
+					try {
+						start();
+					} catch (Exception e1) {System.out.println(e1.getMessage());}
+				}
+			}
+		});
+	}
 	
 	
-	public static void main(String[] args) throws Exception	{
+	public void start() throws Exception {
+		
+		System.out.println("Robot's online");
+		  
+		
+		values = new int[5];							//For storing the finger-values
+		DatagramSocket serverSocket = new DatagramSocket(9875);
+		byte[] recData = new byte[20];
+		System.out.print("Server running..");
+		
+		  
+		JFrame frame = new JFrame("Debugging Prototype");
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		JButton button = new JButton();
+		button.setText("Start");
+		panel.add(button);
+		
+		JLabel thumb = new JLabel("Thumb: ");
+		JLabel index = new JLabel("Index: ");
+		JLabel middle = new JLabel("Middle: ");
+		JLabel ring = new JLabel("Ring: ");
+		JLabel pinky = new JLabel("Pinky: ");
+		panel.add(thumb);
+		panel.add(index);
+		panel.add(middle);
+		panel.add(ring);
+		panel.add(pinky);
+		
+		frame.add(panel);
+		frame.setSize(300, 300);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 		
 
-		  Robot robot = new Robot();
-		  System.out.println("Robot's online");
-		  
-		  try{
 		
-	      System.out.print("Server running..");
-		  int [] values = new int[5];							//For storing the finger-values
-		  DatagramSocket serverSocket = new DatagramSocket(9876);
-		  byte[] recData = new byte[20];
-		  while(true){
-			  DatagramPacket receivePacket = new DatagramPacket(recData, recData.length);
-              serverSocket.receive(receivePacket);
-              String sentence = new String( receivePacket.getData());
-              System.out.println("RECEIVED: " + sentence);
-              
-              values = decodeReceivedString(sentence);
-              
-              robot.readinput(values);
-              /*
-              for(int i = 0; i<5; i++){
-            	  System.out.println("testprint..");
-            	  System.out.println(i +": "+ values[i]);
-              }
-              
-              String gesture ="";
-	   			switch(getGesture(values)){
-   		   		case 1: gesture = "Left";
-   		   		break;
-   		   		case 2: gesture = "Forward";
-   		   		break;
-   		   		case 3: gesture = "Right";
-   		   		break;
-   		   		case 4: gesture = "Stop";
-   		   		break;
-   		   		default: gesture = "no gesture detected";
-   		   }
-	   			System.out.println("gesture: " + gesture);*/
-              
-		  }
-		  
-		} catch (Exception e) {
-			e.printStackTrace();
+		button.addActionListener(new ActionListener() { 
+			public void actionPerformed(ActionEvent e) { 
+				if(success)
+		        {
+		            success = false;
+		            button.setText("Terminate");
+		            
+		            new Thread(
+		              new Runnable() {
+		                public void run() {
+		                  while(!success) {
+		                	DatagramPacket receivePacket = new DatagramPacket(recData, recData.length);
+		                    try {
+								serverSocket.receive(receivePacket);
+							} catch (IOException e1) {e1.printStackTrace();}
+		                    String sentence = new String( receivePacket.getData());
+		                    System.out.println("RECEIVED: " + sentence);
+		                      
+		                    values = decodeReceivedString(sentence);
+		                    
+		                    thumb.setText("Thumb: "+values[0]);
+		                    index.setText("Thumb: "+values[1]);
+		                    middle.setText("Thumb: "+values[2]);
+		                    ring.setText("Thumb: "+values[3]);
+		                    pinky.setText("Thumb: "+values[4]);
+		                        
+		                    try {
+								robot.readinput(values);
+							} catch (RemoteException e) {e.printStackTrace();}
+		                    
+		                    
+		                    
+		                    /*
+		                    for(int i = 0; i<5; i++){
+		                    	System.out.println("testprint..");
+		                    	System.out.println(i +": "+ values[i]);
+		                    }
+		                        
+		                    String gesture ="";
+		          	   		switch(getGesture(values)){
+		             			case 1: gesture = "Left";
+		             			break;
+		             			case 2: gesture = "Forward";
+		             			break;
+		             			case 3: gesture = "Right";
+		             			break;
+		             			case 4: gesture = "Stop";
+		             			break;
+		             			default: gesture = "no gesture detected";
+		             		}
+		          	   		
+		          	   		System.out.println("gesture: " + gesture);*/
+		                        
+		                  }
+		                }
+		              }
+		            ).start();
+		        }
+		        else
+		        {
+		        	success = true;
+		            close(serverSocket);
+		            // As you're controlling the playing value in the Thread,
+		            // setting it here to false would mean that your thread stops too.
+		        }
+			} 
+		});
 		
-		} finally {
-			robot.close();
-		}
+		
+		/*final Thread mainThread = Thread.currentThread();
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				System.out.println("terminate");
+				serverSocket.close();
+				running = false;
+				try {
+					mainThread.join();
+				}
+				catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});*/
 		  
 		  
 	}
@@ -74,6 +202,7 @@ public class MainServer {
 	 * @return integer array with extracted values
 	 */
 	
+	//obsolete
 	private static int getGesture(int[] values){
 		int threshold = 100;
 		int id = -1;
@@ -133,8 +262,15 @@ public class MainServer {
 	}
 	
 	
-	
-	
-	
+	public void close(DatagramSocket serverSocket) {
+		System.out.println("Terminate");
+		serverSocket.close();
+		try {
+			robot.close();
+		} catch (Exception e1) {e1.printStackTrace();}
+		System.exit(1);
 	}
+	
+	
+}
 
--- MainServer/src/mainServer/Robot.java
+++ MainServer/src/mainServer/Robot.java
@@ -3,9 +3,11 @@ package mainServer;
 import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.concurrent.TimeUnit;
 
 import lejos.hardware.BrickFinder;
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.port.Port;
 import lejos.remote.ev3.RMIRegulatedMotor;
 import lejos.remote.ev3.RemoteEV3;
 
@@ -15,14 +17,9 @@ public class Robot {
 	private RMIRegulatedMotor B, C;
 	
 	
-	public Robot() throws RemoteException {
+	public Robot(String ip) throws RemoteException, MalformedURLException, NotBoundException {
 		
-		/*try {
-			ev3 = new RemoteEV3(ip);
-		} catch (RemoteException | MalformedURLException | NotBoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+		ev3 = new RemoteEV3(ip);
 		
 		ev3 = (RemoteEV3) BrickFinder.getDefault();
 		ev3.setDefault();
@@ -31,8 +28,10 @@ public class Robot {
 		LCD.drawString("rollen", 0,5);
 		LCD.drawString("Sie hassen", 0, 6);
 		
+
 		B = ev3.createRegulatedMotor("B", 'L');
-	    C = ev3.createRegulatedMotor("C", 'L');
+		C = ev3.createRegulatedMotor("C", 'L');
+		
 	}
 	
 	public void readinput(int[] input) throws RemoteException {
@@ -40,10 +39,10 @@ public class Robot {
 		if (input[1] < 100) {
 			if (input[0] > 100 && input[2] > 100 && input[3] > 100 && input[4] > 100) {
 				input[1] = (100-input[1])*7;
-				B.backward();
-				C.backward();
 				B.setSpeed(input[1]);
 				C.setSpeed(input[1]);
+				B.backward();
+				C.backward();
 			}
 		}
 		
@@ -54,9 +53,12 @@ public class Robot {
 	}
 	
 	public void close() throws RemoteException	{
+		System.out.println("Closing robot");
 		B.stop(false);
 		C.stop(false);
-		
+		B.close();
+		C.close();
+		System.out.println("done");
 	}
 	
 	
